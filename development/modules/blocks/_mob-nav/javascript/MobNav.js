ef9f5a77bd8dcb1547084c06e0a8ddb71d3cf04d
tk91 <dbreen91@gmail.com>
2016-01-25 10:21:04 +0000

    fixed jade

--- development/modules/blocks/_mob-nav/javascript/MobNav.js
+++ development/modules/blocks/_mob-nav/javascript/MobNav.js
@@ -19,13 +19,13 @@ class MobNav{
 	}
 
 	changeMobNavState(){
-
+		//Open or close the mobile menu
 		this.pageFunctions.toggleState(this.mobNav);
 
 	}
 
 	hideMobNavEvent(){
-
+		//Close the mobile menu
 		this.pageFunctions.changeState(this.mobNav, 'dormant');
 
 	}
@@ -33,10 +33,12 @@ class MobNav{
 
 	addUIEvents(){
 
+		//open or close the mobile menu when the menu button is clicked
 		this.mobNavBtn.click(function(){
 			this.changeMobNavState();
 		}.bind(this));
 
+		//If a click is detected that is outside of the menu button or the mobile menu, close the menu
 		this.doc.click(function(event){
 			if(!$(event.target).closest(this.mobNav).length && !$(event.target).closest(this.mobNavBtn).length){
 				this.hideMobNavEvent();
